baaead36bfa1ecdd310405e81abc1a75af0194de
@edmilsonjosesilverio6 <edmilsondaibinho0@gmail.com>
2022-08-15 13:36:18 +0100

    test: ensure main-router.ts retrun 200

---
+++ src/presentation/routers/main-router.spec.ts
@@ -0,0 +1,16 @@
+import server from '../../server'
+import mainRouter from './main-router'
+import request from 'supertest'
+
+describe('Get / - a simple api endpoint', () => {
+  beforeAll(() => {
+    server.use('/', mainRouter())
+  })
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+  test('Should return Status: 200 and Message', async () => {
+    const response = await request(server).get('/')
+    expect(response.status).toBe(200)
+  })
+})
