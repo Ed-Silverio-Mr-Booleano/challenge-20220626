42685b03a4f06ea644b82176394abcab5a35320a
@edmilsonjosesilverio6 <edmilsondaibinho0@gmail.com>
2022-08-15 22:35:06 +0100

    test: ensure should return 200 on get one product

--- src/presentation/routers/product-router.spec.ts
+++ src/presentation/routers/product-router.spec.ts
@@ -57,4 +57,24 @@ describe('Product Router', () => {
       expect(response.body).toStrictEqual({ message: 'Error fetching data' })
     })
   })
+  describe('GET /products/:code', () => {
+    test('should return 200 with data', async () => {
+      const ExpectedData = {
+        code: 3661112502850,
+        barcode: '3661112502850(EAN / EAN-13)',
+        status: 'imported',
+        imported_t: '2020-02-07T16:00:00Z',
+        url: 'https://world.openfoodfacts.org/product/3661112502850',
+        product_name: 'Jambon de Paris cuit à',
+        quantity: '240 g',
+        categories: 'Meats, Prepared meats, Hams, White hams',
+        packaging: 'Film en plastique, Film en plastique',
+        brands: 'Tradilège, Marque Repère',
+        image_url: 'https://static.openfoodfacts.org/images/products/366/111/250/2850/front_fr.3.400.jpg'
+      }
+      jest.spyOn(mockGetOneProductsUseCase, 'execute').mockImplementation(async () => await Promise.resolve(ExpectedData))
+      const response = await request(server).get('/products/:code')
+      expect(response.status).toBe(200)
+    })
+  })
 })
