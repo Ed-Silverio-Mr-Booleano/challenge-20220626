4157671fa91a4f01199087f0225383a81629b358
@edmilsonjosesilverio6 <edmilsondaibinho0@gmail.com>
2022-08-16 15:36:56 +0100

    feat: ensure mongodb-data-source

---
+++ src/data/data-sources/mongodb/mongodb-product-data-source.ts
@@ -0,0 +1,50 @@
+import { ProductEntity } from '../../../domain/entities/product'
+import { ProductDataSource } from '../../interfaces/data-source/product-data-source'
+import { NoSQLDatabaseWrapper } from '../../interfaces/data-source/nosql-database-data-source'
+
+export class MongoDBProductDataSource implements ProductDataSource {
+  private readonly db: NoSQLDatabaseWrapper
+  constructor (db: NoSQLDatabaseWrapper) {
+    this.db = db
+  }
+
+  async getOne (code: number): Promise<ProductEntity> {
+    const result = await this.db.find({ _id: code })
+    return result.map(item => ({
+      code: item._id,
+      barcode: item.barcode,
+      status: item.status,
+      imported_t: item.imported_t,
+      url: item.url,
+      product_name: item.product_name,
+      quantity: item.quantity,
+      categories: item.categories,
+      packaging: item.packaging,
+      brands: item.brands,
+      image_url: item.image_url
+
+    }))[0]
+  }
+
+  async getAll (): Promise<ProductEntity[]> {
+    const result = await this.db.find({})
+    return result.map(item => ({
+      code: item._id,
+      barcode: item.barcode,
+      status: item.status,
+      imported_t: item.imported_t,
+      url: item.url,
+      product_name: item.product_name,
+      quantity: item.quantity,
+      categories: item.categories,
+      packaging: item.packaging,
+      brands: item.brands,
+      image_url: item.image_url
+
+    }))
+  }
+
+  async create (product: ProductEntity): Promise<void> {
+    await this.db.insertOne(product)
+  }
+}
