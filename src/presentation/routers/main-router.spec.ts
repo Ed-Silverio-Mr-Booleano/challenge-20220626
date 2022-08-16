2c9749e77bffd10725f0079e1e469aedd355bd42
@edmilsonjosesilverio6 <edmilsondaibinho0@gmail.com>
2022-08-16 19:49:36 +0100

    test: ensure should return 200 and message

--- src/presentation/routers/main-router.spec.ts
+++ src/presentation/routers/main-router.spec.ts
@@ -12,5 +12,6 @@ describe('Get / - a simple api endpoint', () => {
   test('Should return Status: 200 and Message', async () => {
     const response = await request(server).get('/')
     expect(response.status).toBe(200)
+    expect(response.body).toEqual({ message: 'Fullstack Challenge 20201026' })
   })
 })
