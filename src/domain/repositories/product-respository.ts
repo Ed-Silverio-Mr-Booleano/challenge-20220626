f4e287955a28d5e7943dedd3f062e17aa41a87d6
@edmilsonjosesilverio6 <edmilsondaibinho0@gmail.com>
2022-08-16 19:32:18 +0100

    feat: ensure src/domain/repositories/

---
+++ src/domain/repositories/product-respository.ts
@@ -0,0 +1,24 @@
+import { ProductDataSource } from '../../data/interfaces/data-source/product-data-source'
+import { ProductEntity } from '../entities/product'
+import { ProductRepository } from '../interfaces/repositories/product-repository'
+
+export class ProductRepositoryImpl implements ProductRepository {
+  productDataSource: ProductDataSource
+  constructor (productDataSource) {
+    this.productDataSource = productDataSource
+  }
+
+  async createProduct (products: ProductEntity): Promise<void> {
+    await this.productDataSource.create(products)
+  }
+
+  async getProduct (code: number): Promise<ProductEntity> {
+    const result = await this.productDataSource.getOne(code)
+    return result
+  }
+
+  async getProducts (): Promise<ProductEntity[]> {
+    const result = await this.productDataSource.getAll()
+    return result
+  }
+}
