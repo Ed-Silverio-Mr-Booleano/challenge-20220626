e723cab72f96e34cc9228d8095d670c8978593b6
@edmilsonjosesilverio6 <edmilsondaibinho0@gmail.com>
2022-08-18 01:11:21 +0100

    feat: ensure scraping products

--- src/presentation/scraping/scraping.ts
+++ src/presentation/scraping/scraping.ts
@@ -1,13 +1,45 @@
 import axios from 'axios'
-import cheerio from 'cheerio'
+import { load } from 'cheerio'
 
-const url = 'https://world.openfoodfacts.org/'
+interface SecondPage {
+  barcode: string
+  quantity?: string
+  packaging?: string
+  brands?: string
+  categories?: string
+}
+const url = 'https://world.openfoodfacts.org'
 
 async function getProducts (): Promise<void> {
-  const { data } = await axios.get(url)
-  const $ = cheerio.load(data)
+  await axios.get(url).then(response => {
+    const $ = load(response.data)
+    $('#search_results ul li').each((index, elem) => {
+      if (index < 2) {
+        console.log($(elem).find('a').attr('href').split('/')[2])
+        console.log($(elem).find('a>span').text())
+        console.log($(elem).find('div>img').attr('src'))
+        console.log(url + $(elem).find('a').attr('href'))
 
-  const name = $('#search_results #products_tabs_content #products_all ul#products_match_all li a div.list_product_name').text()
-  console.log(name)
+        getProducts2(url).then(response => {
+          return response
+        })
+        console.log('\n')
+      }
+    })
+  }).catch(error => { console.log('error:', error) })
+}
+
+async function getProducts2 (url: any): Promise<SecondPage> {
+  let barcode: any
+  await axios.get(url).then(response => {
+    const $ = load(response.data)
+    barcode = $('#barcode_paragraph').text()
+    console.log('barcode =>', barcode)
+    console.log('quantity =>', $('#field_quantity_value').text())
+    console.log('packaging =>', $('#field_packaging_value').text())
+    console.log('brands =>', $('#field_brands_value').text())
+    console.log('categories =>', $('#field_categories_value').text())
+  })
+  return barcode
 }
 getProducts()
