271fe34bdcb89390bd28de242d647ae1ae0b901a
@edmilsonjosesilverio6 <edmilsondaibinho0@gmail.com>
2022-08-19 16:04:41 +0100

    refactor: ensure getProducts function

--- src/presentation/scraping/scraping.ts
+++ src/presentation/scraping/scraping.ts
@@ -1,45 +1,50 @@
-import axios from 'axios'
+/* eslint-disable import/no-duplicates */
+/* eslint-disable @typescript-eslint/restrict-plus-operands */
+import axios1 from 'axios'
+import axios2 from 'axios'
 import { load } from 'cheerio'
 
-interface SecondPage {
-  barcode: string
-  quantity?: string
-  packaging?: string
-  brands?: string
-  categories?: string
-}
 const url = 'https://world.openfoodfacts.org'
-
+const ProductsFirstPage = []
 async function getProducts (): Promise<void> {
-  await axios.get(url).then(response => {
+  let code: number, productName: string, urlImg: string, urlP: string, productURL: string
+  await axios1.get(url).then(response => {
     const $ = load(response.data)
-    $('#search_results ul li').each((index, elem) => {
-      if (index < 2) {
-        console.log($(elem).find('a').attr('href').split('/')[2])
-        console.log($(elem).find('a>span').text())
-        console.log($(elem).find('div>img').attr('src'))
-        console.log(url + $(elem).find('a').attr('href'))
+    let counter = 0
+    const qdt = $('search_results ul li').length
 
-        getProducts2(url).then(response => {
-          return response
+    $('#search_results ul li').map(async (index, elem) => {
+      if (index < qdt) {
+        code = Number.parseInt($(elem).find('a').attr('href').split('/')[2])
+        productName = $(elem).find('a>span').text()
+        urlImg = $(elem).find('div>img').attr('src')
+        productURL = $(elem).find('a').attr('href')
+        urlP = url + productURL
+        await axios2.get(urlP).then(response => {
+          const $2 = load(response.data)
+          const barcode = $2('#barcode_paragraph').text().replace('\n', '').trim()
+          const quantity = $2('#field_quantity_value').text()
+          const packaging = $2('#field_packaging_value').text()
+          const brands = $2('#field_brands_value').text()
+          const categories = $2('#field_categories_value').text()
+
+          ProductsFirstPage.push({
+            code,
+            product_name: productName,
+            url: urlP,
+            barcode,
+            img_url: urlImg,
+            quantity,
+            packaging,
+            brands,
+            categories
+          })
+          counter++
+          if (counter === qdt) return console.log('products =>', ProductsFirstPage)
         })
-        console.log('\n')
       }
     })
   }).catch(error => { console.log('error:', error) })
 }
 
-async function getProducts2 (url: any): Promise<SecondPage> {
-  let barcode: any
-  await axios.get(url).then(response => {
-    const $ = load(response.data)
-    barcode = $('#barcode_paragraph').text()
-    console.log('barcode =>', barcode)
-    console.log('quantity =>', $('#field_quantity_value').text())
-    console.log('packaging =>', $('#field_packaging_value').text())
-    console.log('brands =>', $('#field_brands_value').text())
-    console.log('categories =>', $('#field_categories_value').text())
-  })
-  return barcode
-}
 getProducts()
