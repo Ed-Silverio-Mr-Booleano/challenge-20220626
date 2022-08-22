590898e086956017cb6a355e49e763c73c16f513
@edmilsonjosesilverio6 <edmilsondaibinho0@gmail.com>
2022-08-22 01:02:53 +0100

    refactor: add new functionalities

--- src/presentation/cron/cron.ts
+++ src/presentation/cron/cron.ts
@@ -1,13 +1,22 @@
+/* eslint-disable @typescript-eslint/restrict-plus-operands */
+/* eslint-disable @typescript-eslint/space-infix-ops */
 import { CronJob } from 'cron'
 import getProducts from '../scraping/scraping'
 import { CreateProductUseCase } from '../../domain/interfaces/use-cases/create-product-use-case'
 
+let count = 9
+let url = 'https://world.openfoodfacts.org/' + count
 export default function ProductCron (createProductUseCase: CreateProductUseCase): void {
   const job = new CronJob('*/30 * * * * *', async () => {
-    console.log('Agendou')
-    await getProducts().then((response) => {
+    await getProducts(url).then((response) => {
       if (response.length !== 0) {
         createProductUseCase.execute(response)
+        console.log('Agendou')
+        count++
+        url = 'https://world.openfoodfacts.org/' + count
+        console.log('count:', url)
+      } else {
+        console.log('resposta vazia')
       }
     })
   })
--- src/presentation/scraping/scraping.ts
+++ src/presentation/scraping/scraping.ts
@@ -6,26 +6,30 @@ import axios2 from 'axios'
 import { load } from 'cheerio'
 import moment from 'moment'
 import { ProductEntity } from '../../domain/entities/product'
+import { save } from '../stream/file'
 
-const url = 'https://world.openfoodfacts.org'
-const ProductsFirstPage = []
+// const url = 'https://world.openfoodfacts.org/4'
+
 let Products = []
-export default async function getProducts (): Promise<ProductEntity[]> {
-  let code: number, productName: string, urlImg: string, urlP: string, productURL: string
+export default async function getProducts (url: any): Promise<ProductEntity[]> {
+  const ProductsFirstPage = []
+  // let code: number, productName: string, urlImg: string, urlP: string, productURL: string
   await axios1.get(url).then(response => {
     const $ = load(response.data)
     let counter = 0
     // const qtd = $('search_results ul li').html().length
     // console.log('qtd =>', qtd)
 
-    $('#search_results ul li').map(async (index, elem) => {
+    $('#search_results ul li').each((index, elem) => {
       if (index < 2) {
-        code = Number.parseInt($(elem).find('a').attr('href').split('/')[2])
-        productName = $(elem).find('a>span').text()
-        urlImg = $(elem).find('div>img').attr('src')
-        productURL = $(elem).find('a').attr('href')
-        urlP = url + productURL
-        await axios2.get(urlP).then(response => {
+        const code = Number.parseInt($(elem).find('a').attr('href').split('/')[2])
+        const productName = $(elem).find('a>span').text()
+        const urlImg = $(elem).find('div>img').attr('src')
+        const productURL = $(elem).find('a').attr('href')
+        const urlP = url.slice(0, 31) + productURL
+        console.log('Url Product', productURL)
+        console.log('urlP', urlP)
+        axios2.get(urlP).then(response => {
           const $2 = load(response.data)
           const barcode = $2('#barcode_paragraph').text().replace('\n', '').trim()
           const quantity = $2('#field_quantity_value').text()
@@ -50,6 +54,8 @@ export default async function getProducts (): Promise<ProductEntity[]> {
           counter++
           if (counter == 2) {
             Products = ProductsFirstPage
+            console.log(ProductsFirstPage)
+            save(ProductsFirstPage)
             return ProductsFirstPage
           }
         })
